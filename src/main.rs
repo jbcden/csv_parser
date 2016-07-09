498d5eacb652481cf077c49e90641508311b4d82
Jacob Chae <jbcden@gmail.com>
2016-07-09 15:10:29 -0400

    SAVEPOINT

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,81 @@
-use std::fs::File;
-use std::io::Read;
 use std::io::BufReader;
 use std::io::BufRead;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::sync::mpsc;
-use std::sync::mpsc::{Sender};
-use std::io::{Error};
+// use std::io::Read;
+// use std::fs::File;
+// use std::sync::mpsc::{Sender};
+// use std::io::{Error};
+
+const F_LEN: i32 = 1411070052;
 
 fn main() {
     let count = Arc::new(Mutex::new(0));
     let (tx, rx) = mpsc::channel();
 
-    let mut csv = std::fs::File::open("../FINAL.csv").unwrap();
-    let mut buf_read = BufReader::new(&csv);
-    let lines = buf_read.lines();
-    let line_nums = lines.count();
+    let csv = match std::fs::File::open("../FINAL.csv") {
+        Ok(f) => f,
+        Err(_) => std::fs::File::open("../FINAL.csv").unwrap(),
+    };
+    let buf_read = Arc::new(Mutex::new(BufReader::new(&csv)));
+    // let mut lines = buf_read.lines().by_ref();
+    // let line_nums = lines.count();
 
-    println!("NUM LINES: {}", line_nums);
-    for line in lines {
 
-        process_line(line, count, &tx);
+    //for line in lines {
+    //while reader.read_line(&mut buf).unwrap() > 0 {
+    for x in 0..F_LEN {
+        let mut reader = match buf_read.lock() {
+            Ok(reader) => reader,
+            Err(_) => continue,
+        };
+        let mut buf = String::new();
+        reader.read_line(&mut buf);
+        {
+            let buf = buf.clone();
+            let (count, tx) = (count.clone(), tx.clone());
+            thread::spawn(move || {
+            //     let length = match line {
+            //         Ok(l) => parse_line(l),
+            //         Err(_) => 0,
+            //     };
+                // println!("BUF: {}", buf);
+                let length = parse_line(buf);
+                // println!("LEN: {}", length);
+
+                let count: i32 = match count.lock() {
+                    Ok(c) => *c,
+                    Err(_) => 0,
+                };
+                let answer = count + length;
+
+                match tx.send(answer) {
+                    Ok(_) => (),
+                    Err(_) => println!("ERROR!!!!!"),
+                };
+                //buf.clear();
+            });
+        }
+        if x % 1_000_000 == 0 {
+            println!("X: {}", x);
+        }
     }
 
     println!("PANDA TEST");
-    for _ in 0..line_nums {
+    for _ in 0..F_LEN {
         let res = match rx.recv() {
             Ok(n) => n,
             Err(_) => -2
         };
-        println!("{}", res);
+        println!("RES: {}", res);
     }
 
     // Total: 617_806_098
     // println!("Total: {}", count);
 }
 
-fn process_line(line: Result<String, std::io::Error>, count: Arc<Mutex<i32>>, tx: &std::sync::mpsc::Sender<i32>) {
-    let (count, tx) = (count.clone(), tx.clone());
-    thread::spawn( move || {
-        let length = match line {
-            Ok(l) => parse_line(l),
-            Err(_) => 0,
-        };
-
-        let mut count = count.lock().unwrap();
-        let answer = *count + length;
-
-        tx.send(answer).unwrap();
-    });
-}
+// fn process_line(line: Result<String, std::io::Error>, count: Arc<Mutex<i32>>, tx: &std::sync::mpsc::Sender<i32>) {
 
 
 fn parse_line(line: String) -> i32 {
@@ -64,6 +90,7 @@ fn parse_line(line: String) -> i32 {
 }
 
 // efficient_unsafe(line);
+#[allow(dead_code)]
 fn efficient_unsafe(line: Result<String, String>) -> i32 {
     let unwrapped_line = line.unwrap();
     unwrapped_line.split(',')
